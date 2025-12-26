ec6f7ef246f7efdbc3688e623b77532c40451aaa
DuongNManh <manhduonglhp4@gmail.com>
2025-12-26 12:41:00 -0500

    feat:hehehehe

--- server/djangoapp/restapis.py
+++ server/djangoapp/restapis.py
@@ -1,5 +1,5 @@
 # Uncomment the imports below before you add the function code
-# import requests
+import requests
 import os
 from dotenv import load_dotenv
 
@@ -11,12 +11,46 @@ sentiment_analyzer_url = os.getenv(
     'sentiment_analyzer_url',
     default="http://localhost:5050/")
 
-# def get_request(endpoint, **kwargs):
-# Add code for get requests to back end
+def get_request(endpoint, **kwargs):
+    params = ""
+    if(kwargs):
+        for key,value in kwargs.items():
+            params=params+key+"="+value+"&"
 
-# def analyze_review_sentiments(text):
-# request_url = sentiment_analyzer_url+"analyze/"+text
-# Add code for retrieving sentiments
+    request_url = backend_url+endpoint+"?"+params
 
-# def post_review(data_dict):
-# Add code for posting review
+    print("GET from {} ".format(request_url))
+    try:
+        # Call get method of requests library with URL and parameters
+        response = requests.get(request_url)
+        return response.json()
+    except:
+        # If any error occurs
+        print("Network exception occurred")
+
+def analyze_review_sentiments(text):
+    # Tạo URL gọi tới sentiment analyzer service
+    request_url = sentiment_analyzer_url + "analyze/" + text
+    print("GET from {} ".format(request_url))
+    try:
+        response = requests.get(request_url)
+        return response.json()
+    except Exception as e:
+        print("Network exception occurred:", e)
+        return {"error": "Sentiment analysis failed"}
+
+def post_review(data_dict):
+    # Tạo URL gọi tới backend service để insert review
+    request_url = backend_url + "/insert_review"
+    print("POST to {} ".format(request_url))
+    try:
+        response = requests.post(
+            request_url,
+            json=data_dict,   # gửi dữ liệu dạng JSON
+            headers={"Content-Type": "application/json"}
+        )
+        return response.json()
+    except Exception as e:
+        print("Network exception occurred:", e)
+        return {"error": "Posting review failed"}
+
